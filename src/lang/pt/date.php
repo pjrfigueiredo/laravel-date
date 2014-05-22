6f06f819bd9cb3f811f38e267e1b776e437e1622
pjrfigueiredo <pjrfigueiredo@gmail.com>
2014-05-22 13:41:06 +0100

    The missing! portuguese translation! :)
    tnks

---
+++ src/lang/pt/date.php
@@ -0,0 +1,29 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    | Portuguese Language file by
+    | Paulo Dias :: twitter.com/pauloalexandr
+    | www.creativeoo.com
+
+    */
+
+    'ago'       => 'atrás :time',
+    'from now'  => 'daqui a :time',
+    'year'      => '1 ano|:number anos',
+    'month'     => '1 mês|:number meses',
+    'week'      => '1 semana|:number semanas',
+    'day'       => '1 dia|:number dias',
+    'hour'      => '1 hora|:number horas',
+    'minute'    => '1 minuto|:number minutos',
+    'second'    => '1 segundo|:number segundos'
+
+);	
